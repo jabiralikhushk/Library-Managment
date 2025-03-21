aa80b607df7edc2e07ce9ba9c7a1c6416ee0aa68
Jabir ali <163226601+jabiralikhushk@users.noreply.github.com>
2025-03-22 03:10:26 +0500

    class.py

--- class.py
+++ class.py
@@ -307,10 +307,10 @@ def some_function():
 if some_condition:
     # Indent the function correctly inside the if block
     def some_function():
-      def add_book_form():
-    title = st.text_input("Book title", max_chars=100)  # Indent this line correctly
-    author = st.text_input("Author", max_chars=100)
-    # Further form code here
+   def add_book_form():
+    title = st.text_input("Book title", max_chars=100)  # Properly indented
+    author = st.text_input("Author", max_chars=100)  # Properly indented
+    # Further form handling code here
 
 
                             publication_year = st.number_input 
